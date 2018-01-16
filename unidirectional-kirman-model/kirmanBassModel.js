f0a4504272c1a7778910137acbc8fe048b46c1ae
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-01-16 18:24:08 +0200

    linting kirmanBassModel

--- unidirectional-kirman-model/kirmanBassModel.js
+++ unidirectional-kirman-model/kirmanBassModel.js
@@ -22,31 +22,30 @@ kirmanBassModel.prototype.setTolerance=function(a){this.tolerance=a;};
 //--special setting function
 kirmanBassModel.prototype.updateStep=function() {this.stepDt=this.kappa/(this.population*(2.0*this.e1+this.h)*(1.0+this.tolerance));};
 //--runtime functions
-kirmanBassModel.prototype.reset=function(iDt) {this.customers=0;this.pirates=0;this.totalTime=0;}
 kirmanBassModel.prototype.predict=function() {
-	var exppqt=Math.exp((this.e1+this.h)*this.totalTime);
-	return this.population*this.e1*(exppqt-1.0)/(exppqt*this.e1+this.h);
-}
+    var exppqt=Math.exp((this.e1+this.h)*this.totalTime);
+    return this.population*this.e1*(exppqt-1.0)/(exppqt*this.e1+this.h);
+};
 kirmanBassModel.prototype.stepPerTime=function(iDt) {
-	if(typeof iDt==="undefined") iDt=this.integrationDt;
-	var t=0;
-	while(t<iDt) {
-		var vdt=Math.min(iDt-t,this.stepDt);
-		this.singleInteraction(vdt);
-		t+=vdt;
-	}
-	this.totalTime+=iDt;
-	var curUsr=this.customers+this.pirates+this.charity;
-	if(this.population<=curUsr) return false;
-	return true;
-}
+    if(typeof iDt==="undefined") iDt=this.integrationDt;
+    var t=0;
+    while(t<iDt) {
+        var vdt=Math.min(iDt-t,this.stepDt);
+        this.singleInteraction(vdt);
+        t+=vdt;
+    }
+    this.totalTime+=iDt;
+    var curUsr=this.customers+this.pirates+this.charity;
+    if(this.population<=curUsr) return false;
+    return true;
+};
 kirmanBassModel.prototype.singleInteraction=function(stepDt) {
-	if(typeof stepDt==="undefined") stepDt=this.stepDt;
-	var curUsr=this.customers+this.pirates+this.charity;
-	if(this.population<=curUsr) return false;
-	var probPlus=(this.population-curUsr)*(this.e1+this.h*curUsr/this.population)*stepDt;
-	if(Math.random()<probPlus) this.customers++;
-	probPlus=this.tolerance*probPlus;
-	if(Math.random()<probPlus) this.pirates++;
-	return true;
+    if(typeof stepDt==="undefined") stepDt=this.stepDt;
+    var curUsr=this.customers+this.pirates+this.charity;
+    if(this.population<=curUsr) return false;
+    var probPlus=(this.population-curUsr)*(this.e1+this.h*curUsr/this.population)*stepDt;
+    if(Math.random()<probPlus) this.customers+=1;
+    probPlus=this.tolerance*probPlus;
+    if(Math.random()<probPlus) this.pirates+=1;
+    return true;
 };
