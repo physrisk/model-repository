4c73d9de01f0092a32d91ed8f41acbd4f65db662
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-01-16 16:26:48 +0200

    liting kirman-sde

--- kirman-sde/kirmanSdeModel.js
+++ kirman-sde/kirmanSdeModel.js
@@ -8,36 +8,36 @@ kirmanSdeModel.prototype.integrationDt=1e-3;
 kirmanSdeModel.prototype.kappa=1e-6;
 kirmanSdeModel.prototype.boundary=1e-6;
 //functions
-//--setting properties
+//-=1setting properties
 kirmanSdeModel.prototype.setEpsilon1=function (e) {
-	this.e1=e;
+    this.e1=e;
 };
 kirmanSdeModel.prototype.setEpsilon2=function (e) {
-	this.e2=e;
+    this.e2=e;
 };
 kirmanSdeModel.prototype.setEpsilons=function (ee1, ee2) {
-	this.setEpsilon1(ee1);
-	this.setEpsilon2(ee2);
+    this.setEpsilon1(ee1);
+    this.setEpsilon2(ee2);
 };
 kirmanSdeModel.prototype.setState=function (e) {
-	this.frac=e;
+    this.frac=e;
 };
-//--special setting function
+//-=1special setting function
 kirmanSdeModel.prototype.updateStep=function () {
-	this.stepDt=this.kappa/(this.e1+this.e2+1.0);
+    this.stepDt=this.kappa/(this.e1+this.e2+1.0);
 };
-//--runtime functions
+//-=1runtime functions
 kirmanSdeModel.prototype.stepPerTime=function (iDt) {
-	if(typeof iDt==="undefined") iDt=this.integrationDt;
-	var t=0;
-	while(t<iDt) {
-		var vdt=Math.min(iDt-t,this.stepDt);
-		this.singleStep(vdt);
-		t+=vdt;
-	}
-}
+    if(typeof iDt==="undefined") iDt=this.integrationDt;
+    var t=0;
+    while(t<iDt) {
+        var vdt=Math.min(iDt-t,this.stepDt);
+        this.singleStep(vdt);
+        t+=vdt;
+    }
+};
 kirmanSdeModel.prototype.singleStep=function (stepDt) {
-	if(typeof stepDt==="undefined") stepDt=this.stepDt;
-	this.frac=this.frac+(this.e1*(1.0-this.frac)-this.e2*this.frac)*stepDt+Math.sqrt(2.0*this.frac*(1.0-this.frac)*this.stepDt)*commonFunctions.gaussianRandom();
-	this.frac=Math.max(Math.min(this.frac,1.0-this.boundary),this.boundary);
+    if(typeof stepDt==="undefined") stepDt=this.stepDt;
+    this.frac=this.frac+(this.e1*(1.0-this.frac)-this.e2*this.frac)*stepDt+Math.sqrt(2.0*this.frac*(1.0-this.frac)*this.stepDt)*commonFunctions.gaussianRandom();
+    this.frac=Math.max(Math.min(this.frac,1.0-this.boundary),this.boundary);
 };
