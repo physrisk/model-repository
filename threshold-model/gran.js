18ae392433ba9afbc105f65647aa74326be5faae
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-01-16 18:00:51 +0200

    linting threshold-model

--- threshold-model/gran.js
+++ threshold-model/gran.js
@@ -10,45 +10,46 @@ var field=new Array(); /* array with agent types*/
 var fieldState=new Array(); /* array with agent states*/
 var fieldL=40; /* dimension of "auditorium" */
 var fieldSq=1; /* agents in "auditorium" */
-var fieldPx=5; /* pixels to represent agent (square's side length in px) */
+var fieldPx=5; /* pixels to represent agent (square"s side length in px) */
 var thresholds=new Array(); /* array holding thresholds for distinct agent types */
 var typeProb=new Array(); /* array holding probabilities to have an agent of certain type */
-var typeColorsPassive=['rgb(100,40,40)','rgb(40,100,40)','rgb(40,100,100)','rgb(100,40,100)','rgb(100,100,40)']
-var typeColorsActive=['rgb(255,40,40)','rgb(40,255,40)','rgb(40,255,255)','rgb(255,40,255)','rgb(255,255,40)']
+var typeColorsPassive=["rgb(100,40,40)","rgb(40,100,40)","rgb(40,100,100)","rgb(100,40,100)","rgb(100,100,40)"];
+var typeColorsActive=["rgb(255,40,40)","rgb(40,255,40)","rgb(40,255,255)","rgb(255,40,255)","rgb(255,255,40)"];
 var arrProtesters=new Array(); /* history of protest*/
 var curProtesters=0;
 var histJoined=new Array(); /* last joined agent types */
-$(figureDiv).data('plotOptions', {yaxis:{tickDecimals:2,min:0,max:1}});
+$(figureDiv).data("plotOptions", {yaxis:{tickDecimals:2,min:0,max:1}});
 
 function myParseFloat(val) {
-	return parseFloat((""+val).replace(",","."));
+    return parseFloat((""+val).replace(",","."));
 }
 
 function getParameterValues() {
+    var i;
     time=0;
     curProtesters=0;
     arrProtesters=[[0,0]];
-	fieldL=parseInt($('#controlSize').val());
+    fieldL=parseInt($("#controlSize").val());
     /* pre-calculate some often used values */
     fieldSq=fieldL*fieldL;
-    fieldPx=parseInt(Math.floor(200/fieldL))
+    fieldPx=parseInt(Math.floor(200/fieldL));
     /* pre-calc end */
     typeProb=new Array();
     thresholds=new Array();
     var probSum=0;
-    for(var i=0;i<typeColorsPassive.length;i++) {
-        typeProb.push(myParseFloat($('#controlP'+i).val()))
+    for(i=0;i<typeColorsPassive.length;i+=1) {
+        typeProb.push(myParseFloat($("#controlP"+i).val()));
         probSum+=typeProb[i];
-        thresholds.push(parseInt(myParseFloat($('#controlT'+i).val())*fieldSq))
+        thresholds.push(parseInt(myParseFloat($("#controlT"+i).val())*fieldSq));
     }
-    for(var i=0;i<typeColorsPassive.length;i++) {
+    for(i=0;i<typeColorsPassive.length;i+=1) {
         if(probSum!=1) {
             typeProb[i]/=probSum;
-            $('#controlP'+i).val(typeProb[i]);
+            $("#controlP"+i).val(typeProb[i]);
         }
         if(thresholds[i]>=fieldSq) {
             thresholds[i]=fieldSq;
-            $('#controlT'+i).val(1);
+            $("#controlT"+i).val(1);
         }
     }
     populateField();
@@ -57,53 +58,54 @@ function getParameterValues() {
 }
 
 function initialize() {
-	g=$(plotDiv)[0].getContext('2d');
-	histG=$(histDiv)[0].getContext('2d');
-	window.clearInterval(timeoutID);
-	timeoutID=null;
+    g=$(plotDiv)[0].getContext("2d");
+    histG=$(histDiv)[0].getContext("2d");
+    window.clearInterval(timeoutID);
+    timeoutID=null;
 }
 
 function populateField() {
-	field=new Array();
-	fieldState=new Array();
+    var i;
+    field=new Array();
+    fieldState=new Array();
     var cumProb=new Array();
     var sum=0;
-    for(var i=0;i<typeProb.length;i++) {
+    for(i=0;i<typeProb.length;i+=1) {
         sum+=typeProb[i];
         cumProb.push(sum);
     }
-	for(var i=0;i<fieldL;i++) {
-		var tfield=new Array();
-		var tsfield=new Array();
-		for(var j=0;j<fieldL;j++) {
+    for(i=0;i<fieldL;i+=1) {
+        var tfield=new Array();
+        var tsfield=new Array();
+        for(var j=0;j<fieldL;j+=1) {
             var tr=Math.random();
-            for(var k=0;k<cumProb.length;k++) {
+            for(var k=0;k<cumProb.length;k+=1) {
                 if(tr<cumProb[k]) break;
             }
             tfield.push(k);
             tsfield.push(0);
-		}
-		field.push(tfield);
+        }
+        field.push(tfield);
         fieldState.push(tsfield);
-	}
+    }
     histJoined=new Array();
-    for(var i=0;i<80;i++) histJoined.push(-1);
+    for(i=0;i<80;i+=1) histJoined.push(-1);
 }
 
 function drawField() {
-	for(var i=0;i<fieldL;i++) {
-		for(var j=0;j<fieldL;j++) {
+    for(var i=0;i<fieldL;i+=1) {
+        for(var j=0;j<fieldL;j+=1) {
             if(fieldState[i][j]==0) g.fillStyle=typeColorsPassive[field[i][j]];
             else g.fillStyle=typeColorsActive[field[i][j]];
-			g.fillRect(i*fieldPx,j*fieldPx,fieldPx,fieldPx);
-		}
-	}
+            g.fillRect(i*fieldPx,j*fieldPx,fieldPx,fieldPx);
+        }
+    }
     drawHistory();
 }
 
 function drawHistory() {
-    for(var i=0;i<80;i++) {
-        if(histJoined[i]<0) histG.fillStyle='rgb(255,255,255)';
+    for(var i=0;i<80;i+=1) {
+        if(histJoined[i]<0) histG.fillStyle="rgb(255,255,255)";
         else histG.fillStyle=typeColorsActive[histJoined[i]];
         histG.fillRect(i*5,0,5,5);
     }
@@ -114,39 +116,39 @@ function iteration() {
     var j=parseInt(Math.floor(Math.random()*fieldL));
     if(fieldState[i][j]==0 && thresholds[field[i][j]]<=curProtesters) {
         fieldState[i][j]=1;
-        curProtesters++;
+        curProtesters+=1;
         histJoined.push(field[i][j]);
         histJoined=histJoined.slice(1);
     }
 }
 
 function plotFigure() {
-	$.plot($(figureDiv),[{data:arrProtesters,color:"black"}],$(figureDiv).data('plotOptions'));
+    $.plot($(figureDiv),[{data:arrProtesters,color:"black"}],$(figureDiv).data("plotOptions"));
 }
 
 function startGame() {
-	initialize();
+    initialize();
     getParameterValues();
-	$("#resume").click();
+    $("#resume").click();
 }
 
 function nextFrame() {
     time+=1;
-    for(var i=0;i<0.1*fieldSq;i++) iteration();
+    for(var i=0;i<0.1*fieldSq;i+=1) iteration();
     arrProtesters.push([time,curProtesters/fieldSq]);
-	drawField();
-	plotFigure();
-    if(curProtesters==fieldSq) $('#resume').click();
+    drawField();
+    plotFigure();
+    if(curProtesters==fieldSq) $("#resume").click();
 }
 
 /*main*/
 $(function () {
-	initialize();
+    initialize();
     getParameterValues();
-	$("#start").click(function(){startGame();});
-	$("#resume").toggle(function(){resumeGame();},function(){stopGame();});
-    for(var i=0;i<typeColorsActive.length;i++) {
-        $('#controlP'+i+', #controlT'+i).css('background-color',typeColorsActive[i]);
+    $("#start").click(function(){startGame();});
+    $("#resume").toggle(function(){resumeGame();},function(){stopGame();});
+    for(var i=0;i<typeColorsActive.length;i+=1) {
+        $("#controlP"+i+", #controlT"+i).css("background-color",typeColorsActive[i]);
     }
-    $('#controlT0').css('background-color',typeColorsPassive[0]).css('color','white');
+    $("#controlT0").css("background-color",typeColorsPassive[0]).css("color","white");
 });
