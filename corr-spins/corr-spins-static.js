a54ac95dc0137d78bde424d32114010789cafa5e
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-01-16 15:55:14 +0200

    linting corr-spins

--- corr-spins/corr-spins-static.js
+++ corr-spins/corr-spins-static.js
@@ -5,95 +5,96 @@ var NParticles=1000;
 var timer=null;
 var interactionsOccured=0;
 var boundaries=0;
-$('#restart').click(function() {initialize();$('#stopBlock, #stop').show();$('#stop').click();});
-$('#stopBlock, #stop').hide();
+$("#restart").click(function() {initialize();$("#stopBlock, #stop").show();$("#stop").click();});
+$("#stopBlock, #stop").hide();
 
 function plotFigure() {
-	var opts=$('#plotDiv').data('plotOptions');
-	var outDist=new Array();
-	var matlog10=Math.log(10);
-	var norm=0;
-	var min=99999;
-	if(interactionsOccured>0) norm=Math.log(interactionsOccured);
-	for(var i=0;i<trueDist.length;i++) {
-		if(trueDist[i][1]>=1) {
-			var tput=(Math.log(trueDist[i][1])-norm)/matlog10;
-			min=Math.min(min,tput);
-			outDist.push([trueDist[i][0],tput]);
-		}
-	}
-	if(outDist.length>0) opts.yaxis.min=min;
-	rezDist=outDist;
-	$('#plotDiv').data('plotOptions',opts);
-	$.plot($("#plotDiv"),[{data:teorDist, color:"red", label:labels[0]},{data:teor2Dist, color:"magenta", label:labels[1]},{data:outDist,color:"blue",label:labels[2]}],$('#plotDiv').data('plotOptions'));
+    var opts=$("#plotDiv").data("plotOptions");
+    var outDist=new Array();
+    var matlog10=Math.log(10);
+    var norm=0;
+    var min=99999;
+    if(interactionsOccured>0) norm=Math.log(interactionsOccured);
+    for(var i=0;i<trueDist.length;i++) {
+        if(trueDist[i][1]>=1) {
+            var tput=(Math.log(trueDist[i][1])-norm)/matlog10;
+            min=Math.min(min,tput);
+            outDist.push([trueDist[i][0],tput]);
+        }
+    }
+    if(outDist.length>0) opts.yaxis.min=min;
+    var rezDist=outDist;
+    $("#plotDiv").data("plotOptions",opts);
+    $.plot($("#plotDiv"),[{data:teorDist, color:"red", label:labels[0]},{data:teor2Dist, color:"magenta", label:labels[1]},{data:outDist,color:"blue",label:labels[2]}],$("#plotDiv").data("plotOptions"));
 }
 
 function play() {
-	var bPos=[0,NParticles];
-	for(var i=0;i<boundaries;i++) {
-		var npos=Math.floor(NParticles*Math.random());
-		while(bPos.indexOf(npos)>=0) npos=Math.floor(NParticles*Math.random());
-		bPos.push(npos);
-	}
-	bPos=bPos.sort(function(a,b){return a>b;});
-	var rez=0;
-	for(var i=0;i<bPos.length-1;i++) {
-		rez+=(2.0*(i % 2)-1)*(bPos[i+1]-bPos[i]);
-	}
-	rez*=(Math.floor(Math.random()*2)-0.5);
-	var tdId=Math.round(rez+NParticles/2);
-	trueDist[tdId][1]++;
-	interactionsOccured+=1;
+    var i;
+    var bPos=[0,NParticles];
+    for(i=0;i<boundaries;i++) {
+        var npos=Math.floor(NParticles*Math.random());
+        while(bPos.indexOf(npos)>=0) npos=Math.floor(NParticles*Math.random());
+        bPos.push(npos);
+    }
+    bPos=bPos.sort(function(a,b){return a>b;});
+    var rez=0;
+    for(i=0;i<bPos.length-1;i++) {
+        rez+=(2.0*(i % 2)-1)*(bPos[i+1]-bPos[i]);
+    }
+    rez*=(Math.floor(Math.random()*2)-0.5);
+    var tdId=Math.round(rez+NParticles/2);
+    trueDist[tdId][1]++;
+    interactionsOccured+=1;
 }
 
 function la_gamma(x){
-	var p=[0.99999999999980993, 676.5203681218851, -1259.1392167224028, 771.32342877765313, -176.61502916214059, 12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
-	var g=7;
-	if(x<0.5) return Math.PI/(Math.sin(Math.PI*x)*la_gamma(1-x));
-	x-=1;
-	var a=p[0];
-	var t=x+g+0.5;
-	for(var i=1;i<p.length;i++){
-		a+=p[i]/(x+i);
-	}
-	return Math.sqrt(2*Math.PI)*Math.pow(t,x+0.5)*Math.exp(-t)*a;
+    var p=[0.99999999999980993, 676.5203681218851, -1259.1392167224028, 771.32342877765313, -176.61502916214059, 12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
+    var g=7;
+    if(x<0.5) return Math.PI/(Math.sin(Math.PI*x)*la_gamma(1-x));
+    x-=1;
+    var a=p[0];
+    var t=x+g+0.5;
+    for(var i=1;i<p.length;i++){
+        a+=p[i]/(x+i);
+    }
+    return Math.sqrt(2*Math.PI)*Math.pow(t,x+0.5)*Math.exp(-t)*a;
 }
 
 function timeStep() {
-	for(var j=0;j<1000;j++) play();
-	plotFigure();
+    for(var j=0;j<1000;j++) play();
+    plotFigure();
 }
 
 function initialize() {
-	$('#plotDiv').data('plotOptions', {
-		xaxis:{axisLabel:'M'},
-		yaxis:{axisLabel:'lg[p(M)]',min:null}
-	});
-	teorDist=new Array();
-	teor2Dist=new Array();
-	trueDist=new Array();
-	NParticles=parseInt($('#controlN').val());
-	NParticles=2*Math.floor(NParticles/2);
-	$('#controlN').val(NParticles);
-	boundaries=parseInt($('#controlD').val());
-	timer=null;
-	interactionsOccured=0;
-	var la=Math.floor((boundaries-1)/2);
-	var cTeor=Math.log(2*la_gamma(1.5+la)/(NParticles*Math.sqrt(Math.PI)*la_gamma(1+la)));
-	var cTeor2=0.5*Math.log(2/(NParticles*Math.PI));
-	var matlog10=Math.log(10);
-	for(var i=0;i<NParticles;i++) {
-		trueDist.push([-NParticles/2+i,0]);
-		var x=2*i/NParticles-1;
-		teorDist.push([-NParticles/2+i,(cTeor+la*Math.log(1-x*x))/matlog10]);
-		teor2Dist.push([-NParticles/2+i,(cTeor2-(NParticles/2)*x*x)/matlog10]);
-	}
-	plotFigure();
+    $("#plotDiv").data("plotOptions", {
+        xaxis:{axisLabel:"M"},
+        yaxis:{axisLabel:"lg[p(M)]",min:null}
+    });
+    teorDist=new Array();
+    teor2Dist=new Array();
+    trueDist=new Array();
+    NParticles=parseInt($("#controlN").val());
+    NParticles=2*Math.floor(NParticles/2);
+    $("#controlN").val(NParticles);
+    boundaries=parseInt($("#controlD").val());
+    timer=null;
+    interactionsOccured=0;
+    var la=Math.floor((boundaries-1)/2);
+    var cTeor=Math.log(2*la_gamma(1.5+la)/(NParticles*Math.sqrt(Math.PI)*la_gamma(1+la)));
+    var cTeor2=0.5*Math.log(2/(NParticles*Math.PI));
+    var matlog10=Math.log(10);
+    for(var i=0;i<NParticles;i++) {
+        trueDist.push([-NParticles/2+i,0]);
+        var x=2*i/NParticles-1;
+        teorDist.push([-NParticles/2+i,(cTeor+la*Math.log(1-x*x))/matlog10]);
+        teor2Dist.push([-NParticles/2+i,(cTeor2-(NParticles/2)*x*x)/matlog10]);
+    }
+    plotFigure();
 }
 
 function start() {timer=setInterval(function(){timeStep();},100);}
 function stop() {clearInterval(timer);}
 
 $(function () {
-	initialize();plotFigure();
+    initialize();plotFigure();
 });
--- corr-spins/make-one.js
+++ corr-spins/make-one.js
@@ -1,39 +1,40 @@
 var NParticles=100;
 var boundaries=4;
-$('#stop').click(function() {initialize();$("#controlM").val(play());});
+$("#stop").click(function() {initialize();$("#controlM").val(play());});
 
 function play() {
-	var bPos=[0,NParticles];
-	for(var i=0;i<boundaries;i++) {
-		var npos=Math.floor(NParticles*Math.random());
-		while(bPos.indexOf(npos)>=0) npos=Math.floor(NParticles*Math.random());
-		bPos.push(npos);
-	}
-	bPos=bPos.sort(function(a,b){return a>b;});
-	var spin=2*Math.floor(Math.random()*2)-1;
-	var g=$("#plotDiv")[0].getContext('2d');
-	var nB=1;
-	var rez=0;
-	for(var i=0;i<NParticles;i++) {
-		if(i==bPos[nB]) {
-			spin*=(-1);
-			nB++;
-		}
-		rez+=spin;
-		if(spin>0) g.fillStyle="rgb(255,0,0)";
-		else g.fillStyle="rgb(0,0,255)";
-		g.fillRect(i*4,0,4,4);
-	}
-	return rez/2;
+    var i;
+    var bPos=[0,NParticles];
+    for(i=0;i<boundaries;i+=1) {
+        var npos=Math.floor(NParticles*Math.random());
+        while(bPos.indexOf(npos)>=0) npos=Math.floor(NParticles*Math.random());
+        bPos.push(npos);
+    }
+    bPos=bPos.sort(function(a,b){return a>b;});
+    var spin=2*Math.floor(Math.random()*2)-1;
+    var g=$("#plotDiv")[0].getContext("2d");
+    var nB=1;
+    var rez=0;
+    for(i=0;i<NParticles;i+=1) {
+        if(i==bPos[nB]) {
+            spin*=(-1);
+            nB+=1;
+        }
+        rez+=spin;
+        if(spin>0) g.fillStyle="rgb(255,0,0)";
+        else g.fillStyle="rgb(0,0,255)";
+        g.fillRect(i*4,0,4,4);
+    }
+    return rez/2;
 }
 
 function initialize() {
-	NParticles=parseInt($('#controlN').val());
-	NParticles=2*Math.floor(NParticles/2);
-	$('#controlN').val(NParticles);
-	boundaries=parseInt($('#controlD').val());
+    NParticles=parseInt($("#controlN").val());
+    NParticles=2*Math.floor(NParticles/2);
+    $("#controlN").val(NParticles);
+    boundaries=parseInt($("#controlD").val());
 }
 
 $(function () {
-	initialize();$("#controlM").val(play());
+    initialize();$("#controlM").val(play());
 });
