bf9a90cc1fcc1f00e2cc712e892ed68534196feb
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-01-16 16:41:36 +0200

    linting lotka-volterra

--- lotka-volterra-abm/lv-abm.js
+++ lotka-volterra-abm/lv-abm.js
@@ -13,169 +13,170 @@ var wolfReprod=1;
 var wolfDeath=0.02;
 var nWArr=new Array();
 var nRArr=new Array();
-$('#popDiv').data('plotOptions', {xaxis:{show:false},yaxis:{show:false}});
+$("#popDiv").data("plotOptions", {xaxis:{show:false},yaxis:{show:false}});
 
 function initialize() {
-	readParameters();
-	g=$("#plotDiv")[0].getContext('2d');
-	time=0;
-	nWArr=[[time,Nwolves]];
-	nRArr=[[time,Nrabbits]];
-	window.clearInterval(timeoutID);
-	timeoutID=null;
-	populateField();
-	drawField();
-	plotFigure();
+    readParameters();
+    g=$("#plotDiv")[0].getContext("2d");
+    time=0;
+    nWArr=[[time,Nwolves]];
+    nRArr=[[time,Nrabbits]];
+    window.clearInterval(timeoutID);
+    timeoutID=null;
+    populateField();
+    drawField();
+    plotFigure();
 }
 
 function plotFigure() {
-	$.plot($("#popDiv"),[{data:nWArr, color:"black"},{data:nRArr, color:"light gray"}],$('#popDiv').data('plotOptions'));
+    $.plot($("#popDiv"),[{data:nWArr, color:"black"},{data:nRArr, color:"light gray"}],$("#popDiv").data("plotOptions"));
 }
 
 function myParseFloat(val) {
-	return parseFloat((""+val).replace(",","."));
+    return parseFloat((""+val).replace(",","."));
 }
 
 function readParameters() {
-	Nwolves=parseInt($("#wolfPop").val());
-	Nrabbits=parseInt($("#rabPop").val());
-	rabitReprod=myParseFloat($("#rabRep").val())/100.0;
-	wolfReprod=myParseFloat($("#wolfRep").val())/100.0;
-	wolfDeath=myParseFloat($("#wolfDeath").val())/100.0;
+    Nwolves=parseInt($("#wolfPop").val());
+    Nrabbits=parseInt($("#rabPop").val());
+    rabitReprod=myParseFloat($("#rabRep").val())/100.0;
+    wolfReprod=myParseFloat($("#wolfRep").val())/100.0;
+    wolfDeath=myParseFloat($("#wolfDeath").val())/100.0;
 }
 
 function iteracija() {
-	time++;
-	for(var i=0;i<iterations;i++) vienaIteracija();
-	if(time>400) {
-		nWArr.splice(0,1);
-		nRArr.splice(0,1);
-	}
-	nWArr.push([time,Nwolves]);
-	nRArr.push([time,Nrabbits]);
-	drawField();
-	plotFigure();
-	$("#TInd").text(time);
-	$("#RInd").text(Nrabbits);
-	$("#WInd").text(Nwolves);
-	if(Nwolves<1 || Nrabbits<1) $("#start").click().attr("disabled","disabled");
+    time+=1;
+    for(var i=0;i<iterations;i+=1) vienaIteracija();
+    if(time>400) {
+        nWArr.splice(0,1);
+        nRArr.splice(0,1);
+    }
+    nWArr.push([time,Nwolves]);
+    nRArr.push([time,Nrabbits]);
+    drawField();
+    plotFigure();
+    $("#TInd").text(time);
+    $("#RInd").text(Nrabbits);
+    $("#WInd").text(Nwolves);
+    if(Nwolves<1 || Nrabbits<1) $("#start").click().attr("disabled","disabled");
 }
 
 function vienaIteracija() {
-	//renkami du agentai - vienas atsitiktinis, kitas jo kaimynas
-	var x1=Math.floor(Math.random()*fieldLX);
-	var y1=Math.floor(Math.random()*fieldLY);
-	var x2=0;
-	var y2=0;
-	while((x2==0)&&(y2==0)) {
-		x2=Math.floor(Math.random()*3)-2;
-		y2=Math.floor(Math.random()*3)-2;
-	}
-	x2+=x1;
-	y2+=y1;
-	while(x2<0) x2+=fieldLX;
-	while(x2>=fieldLX) x2-=fieldLX;
-	while(y2<0) y2+=fieldLY;
-	while(y2>=fieldLY) y2-=fieldLY;
-	//agentai istrinkti ir patikrinti
-	//1. jei vilkas prie zuikio, triusis mirsta, o vilkas gali reprudukuotis
-	if(((field[x1][y1]==1)&&(field[x2][y2]==-1))||((field[x2][y2]==1)&&(field[x1][y1]==-1))) {
-		if(field[x2][y2]==-1) {
-			Nrabbits--;
-			if(Math.random()<wolfReprod) {
-				field[x2][y2]=1;
-				Nwolves++;
-			} else {
-				field[x2][y2]=0;
-			}
-		} else if(field[x1][y1]==-1) {
-			Nrabbits--;
-			if(Math.random()<wolfReprod) {
-				Nwolves++;
-				field[x1][y1]=1;
-			} else {
-				field[x1][y1]=0;
-			}
-		}
-	}
-	//2. jei triusis prie zoles, tai triusis gali reprodukuotis
-	else if(((field[x1][y1]==0)&&(field[x2][y2]==-1))||((field[x2][y2]==0)&&(field[x1][y1]==-1))) {
-		if(Math.random()<rabitReprod) {
-			if(field[x2][y2]==-1) {
-				field[x1][y1]=-1;
-				Nrabbits++;
-			} else if(field[x1][y1]==-1) {
-				field[x2][y2]=-1;
-				Nrabbits++;
-			}
-		}
-	}
-	//3. jei kazkas stovi prie tuscio ploto, tas kazkas pereina i ta langeli, jei vilkas stovi prie tuscio langelio, tai jis gali mirti
-	if((field[x2][y2]==0)&&(field[x1][y1]!=0)) {
-		if(field[x1][y1]==1) {
-			field[x1][y1]=0;
-			if(Math.random()>=wolfDeath) {//vilkas nemirsta
-				field[x2][y2]=1;
-			} else {
-				Nwolves--;
-			}
-		} else {
-			field[x2][y2]=field[x1][y1];
-			field[x1][y1]=0;
-		}
-	} else if((field[x1][y1]==0)&&(field[x2][y2]!=0)) {
-		if(field[x2][y2]==1) {
-			field[x2][y2]=0;
-			if(Math.random()>=wolfDeath) {//vilkas nemirsta
-				field[x1][y1]=1;
-			} else {
-				Nwolves--;
-			}
-		} else {
-			field[x1][y1]=field[x2][y2];
-			field[x2][y2]=0;
-		}
-	}
-	return ;
+    //renkami du agentai - vienas atsitiktinis, kitas jo kaimynas
+    var x1=Math.floor(Math.random()*fieldLX);
+    var y1=Math.floor(Math.random()*fieldLY);
+    var x2=0;
+    var y2=0;
+    while((x2==0)&&(y2==0)) {
+        x2=Math.floor(Math.random()*3)-2;
+        y2=Math.floor(Math.random()*3)-2;
+    }
+    x2+=x1;
+    y2+=y1;
+    while(x2<0) x2+=fieldLX;
+    while(x2>=fieldLX) x2-=fieldLX;
+    while(y2<0) y2+=fieldLY;
+    while(y2>=fieldLY) y2-=fieldLY;
+    //agentai istrinkti ir patikrinti
+    //1. jei vilkas prie zuikio, triusis mirsta, o vilkas gali reprudukuotis
+    if(((field[x1][y1]==1)&&(field[x2][y2]==-1))||((field[x2][y2]==1)&&(field[x1][y1]==-1))) {
+        if(field[x2][y2]==-1) {
+            Nrabbits--;
+            if(Math.random()<wolfReprod) {
+                field[x2][y2]=1;
+                Nwolves+=1;
+            } else {
+                field[x2][y2]=0;
+            }
+        } else if(field[x1][y1]==-1) {
+            Nrabbits--;
+            if(Math.random()<wolfReprod) {
+                Nwolves+=1;
+                field[x1][y1]=1;
+            } else {
+                field[x1][y1]=0;
+            }
+        }
+    }
+    //2. jei triusis prie zoles, tai triusis gali reprodukuotis
+    else if(((field[x1][y1]==0)&&(field[x2][y2]==-1))||((field[x2][y2]==0)&&(field[x1][y1]==-1))) {
+        if(Math.random()<rabitReprod) {
+            if(field[x2][y2]==-1) {
+                field[x1][y1]=-1;
+                Nrabbits+=1;
+            } else if(field[x1][y1]==-1) {
+                field[x2][y2]=-1;
+                Nrabbits+=1;
+            }
+        }
+    }
+    //3. jei kazkas stovi prie tuscio ploto, tas kazkas pereina i ta langeli, jei vilkas stovi prie tuscio langelio, tai jis gali mirti
+    if((field[x2][y2]==0)&&(field[x1][y1]!=0)) {
+        if(field[x1][y1]==1) {
+            field[x1][y1]=0;
+            if(Math.random()>=wolfDeath) {//vilkas nemirsta
+                field[x2][y2]=1;
+            } else {
+                Nwolves--;
+            }
+        } else {
+            field[x2][y2]=field[x1][y1];
+            field[x1][y1]=0;
+        }
+    } else if((field[x1][y1]==0)&&(field[x2][y2]!=0)) {
+        if(field[x2][y2]==1) {
+            field[x2][y2]=0;
+            if(Math.random()>=wolfDeath) {//vilkas nemirsta
+                field[x1][y1]=1;
+            } else {
+                Nwolves--;
+            }
+        } else {
+            field[x1][y1]=field[x2][y2];
+            field[x2][y2]=0;
+        }
+    }
+    return ;
 }
 
 function placeAgentInEmpty(type) {
-	var rX=Math.floor(Math.random()*fieldLX);
-	var rY=Math.floor(Math.random()*fieldLY);
-	while(field[rX][rY]!=0) {
-		rX=Math.floor(Math.random()*fieldLX);
-		rY=Math.floor(Math.random()*fieldLY);
-	}
-	field[rX][rY]=type;
+    var rX=Math.floor(Math.random()*fieldLX);
+    var rY=Math.floor(Math.random()*fieldLY);
+    while(field[rX][rY]!=0) {
+        rX=Math.floor(Math.random()*fieldLX);
+        rY=Math.floor(Math.random()*fieldLY);
+    }
+    field[rX][rY]=type;
 }
 
 function populateField() {
-	field=new Array();
-	var tfield=new Array();
-	for(var j=0;j<fieldLY;j++) tfield.push(0);
-	for(var i=0;i<fieldLX;i++) field.push(tfield.slice(0,fieldLY));
-	for(var i=0;i<Nrabbits;i++) placeAgentInEmpty(-1);
-	for(var i=0;i<Nwolves;i++) placeAgentInEmpty(1);
+    var i;
+    field=new Array();
+    var tfield=new Array();
+    for(i=0;i<fieldLY;i+=1) tfield.push(0);
+    for(i=0;i<fieldLX;i+=1) field.push(tfield.slice(0,fieldLY));
+    for(i=0;i<Nrabbits;i+=1) placeAgentInEmpty(-1);
+    for(i=0;i<Nwolves;i+=1) placeAgentInEmpty(1);
 }
 
 function drawField() {
-	g.fillStyle="rgb(0,255,0)";
-	g.fillRect(0,0,fieldLX*3,fieldLY*3);
-	for(var i=0;i<fieldLX;i++) {
-		for(var j=0;j<fieldLY;j++) {
-			if(field[i][j]==1) {
-				g.fillStyle="rgb(0,0,0)";
-				g.fillRect(i*3,j*3,3,3);
-			} else if(field[i][j]==-1) {
-				g.fillStyle="rgb(255,255,255)";
-				g.fillRect(i*3,j*3,3,3);
-			}
-		}
-	}
+    g.fillStyle="rgb(0,255,0)";
+    g.fillRect(0,0,fieldLX*3,fieldLY*3);
+    for(var i=0;i<fieldLX;i+=1) {
+        for(var j=0;j<fieldLY;j+=1) {
+            if(field[i][j]==1) {
+                g.fillStyle="rgb(0,0,0)";
+                g.fillRect(i*3,j*3,3,3);
+            } else if(field[i][j]==-1) {
+                g.fillStyle="rgb(255,255,255)";
+                g.fillRect(i*3,j*3,3,3);
+            }
+        }
+    }
 }
 
 function kadras() {
-	iteracija();
-	drawField();
-	plotFigure();
+    iteracija();
+    drawField();
+    plotFigure();
 }
--- lotka-volterra/lvModel.js
+++ lotka-volterra/lvModel.js
@@ -10,41 +10,41 @@ lvModel.prototype.x2=12.0;
 lvModel.prototype.kappa=0.1;
 //functions
 lvModel.prototype.setParams=function (aa1, aa2, cc12, cc21, ttmax, x10, x20) {
-	this.a1=aa1;
-	this.a2=aa2;
-	this.c12=cc12;
-	this.c21=cc21;
-	this.tmax=ttmax;
-	this.x1=xx10;
-	this.x2=xx20;
+    this.a1=aa1;
+    this.a2=aa2;
+    this.c12=cc12;
+    this.c21=cc21;
+    this.tmax=ttmax;
+    this.x1=x10;
+    this.x2=x20;
 };
 lvModel.prototype.realization=function () {
-	var rez1=new Array();
-	var rez2=new Array();
-	var t=0;
-	var selStep=this.tmax/300.0;
-	while(t<this.tmax) {
-		var ret=this.step(selStep);
-		rez1.push([t,ret[0]]);
-		rez2.push([t,ret[1]]);
-		t+=selStep;
-	}
-	return [rez1,rez2];
-}
+    var rez1=new Array();
+    var rez2=new Array();
+    var t=0;
+    var selStep=this.tmax/300.0;
+    while(t<this.tmax) {
+        var ret=this.step(selStep);
+        rez1.push([t,ret[0]]);
+        rez2.push([t,ret[1]]);
+        t+=selStep;
+    }
+    return [rez1,rez2];
+};
 lvModel.prototype.step=function (dt) {
-	var t=0;
-	while(t<dt) {
-		var term1=this.a1*this.x1;
-		var term2=this.c12*this.x1*this.x2;
-		var term3=this.a2*this.x2;
-		var term4=this.c21*this.x1*this.x2;
-		var stepSize=Math.max(term1,term2);
-		stepSize=Math.max(stepSize,term3);
-		stepSize=Math.max(stepSize,term4);
-		var innerT=Math.min(this.kappa/stepSize,dt-t);
-		this.x1=this.x1+innerT*(term1-term2);
-		this.x2=this.x2+innerT*(term4-term3);
-		t+=innerT;
-	}
-	return [this.x1,this.x2];
+    var t=0;
+    while(t<dt) {
+        var term1=this.a1*this.x1;
+        var term2=this.c12*this.x1*this.x2;
+        var term3=this.a2*this.x2;
+        var term4=this.c21*this.x1*this.x2;
+        var stepSize=Math.max(term1,term2);
+        stepSize=Math.max(stepSize,term3);
+        stepSize=Math.max(stepSize,term4);
+        var innerT=Math.min(this.kappa/stepSize,dt-t);
+        this.x1=this.x1+innerT*(term1-term2);
+        this.x2=this.x2+innerT*(term4-term3);
+        t+=innerT;
+    }
+    return [this.x1,this.x2];
 };
