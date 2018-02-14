b028f5a1cd180a0ef059a22f334a4c02c80e5582
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-02-14 14:21:59 +0200

    emm OB model: initial commit

---
+++ emm-ob-model/emmModel.js
@@ -0,0 +1,33 @@
+class EmmModel{
+    constructor(marketBuyRate=1,marketSellRate=1,sameSideProb=0.5,reportTick=1) {
+        this.totalMarketRate=marketBuyRate+marketSellRate;
+        this.totalBuyProb=marketBuyRate/this.totalMarketRate;
+        this.sameSideProb=sameSideProb;
+        this.time=0;
+        this.reportAt=0;
+        this.reportTick=reportTick;
+        this.bestSell=0;
+        this.lastPrice=-0.5;
+    }
+    step() {
+        this.reportAt+=this.reportTick;
+        if(this.time>this.reportAt) {
+            return this.lastPrice;
+        }
+        while(this.time<this.reportAt) {
+            this.internalStep();
+        }
+        return this.lastPrice;
+    }
+    internalStep() {
+        var timeTick=-Math.log(Math.random())/this.totalMarketRate;
+        var direction=0;
+        if(Math.random()<this.sameSideProb) {
+            direction=Math.random()<this.totalBuyProb ? +1 : -1;
+        }
+        this.bestSell+=direction;
+        this.lastPrice=this.bestSell-0.5;
+        this.time+=timeTick;
+        return true;
+    }
+}
