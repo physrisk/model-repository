6679fd7a6dc128b1c8009d34d4289f191f7bb79d
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-02-16 15:23:05 +0200

    emm ob model: add deal per step counter

--- emm-ob-model/emmModel.js
+++ emm-ob-model/emmModel.js
@@ -7,12 +7,14 @@ class EmmModel{
         this.reportTick=reportTick;
         this.bestSell=0;
         this.lastPrice=-0.5;
+        this.nDeals=[0,0];// sell, buy
     }
     setRates(marketBuyRate=1,marketSellRate=1) {
         this.totalMarketRate=marketBuyRate+marketSellRate;
         this.totalBuyProb=marketBuyRate/this.totalMarketRate;
     }
     step() {
+        this.nDeals=[0,0];
         this.reportAt+=this.reportTick;
         if(this.time>this.reportAt) {
             return this.lastPrice;
@@ -24,9 +26,15 @@ class EmmModel{
     }
     internalStep() {
         var timeTick=-Math.log(Math.random())/this.totalMarketRate;
+        var buyer=Math.random()<this.totalBuyProb;
+        if(buyer) {
+            this.nDeals[1]+=1;
+        } else {
+            this.nDeals[0]+=1;
+        }
         var direction=0;
         if(Math.random()<this.sameSideProb) {
-            direction=Math.random()<this.totalBuyProb ? +1 : -1;
+            direction=buyer ? +1 : -1;
         }
         this.bestSell+=direction;
         this.lastPrice=this.bestSell-0.5;
