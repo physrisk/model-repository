19299cc0dbb9a356739a1435098cf23ae1c7721e
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-10-21 09:37:57 +0300

    q-voter model: initial commit

---
+++ voter/qVoterModel.js
@@ -0,0 +1,73 @@
+class QVoterModel {
+    constructor(height=100,width=200,q=2,epsi=0,pUp=0.5) {
+        this.width=width;
+        this.height=height;
+        this.q=q;
+        this.epsilon=epsi;
+        this.globalSpin=0;
+        this.initializeSpinArray(pUp);
+    }
+    initializeSpinArray(pUp) {
+        let i,j,tmp;
+        this.spinArray=[];
+        for(i=0;i<this.height;i+=1) {
+            tmp=[];
+            for(j=0;j<this.width;j+=1) {
+                if(Math.random()<pUp) {
+                    tmp.push(1);
+                    this.globalSpin+=1;
+                } else {
+                    tmp.push(-1);
+                    this.globalSpin-=1;
+                }
+            }
+            this.spinArray.push(tmp);
+        }
+    }
+    step(nSteps) {
+        let i;
+        for(i=0;i<nSteps;i+=1) {
+            this.singleStep();
+        }
+        return this.globalSpin;
+    }
+    singleStep() {
+        let x,y,spinZero,qi,united;
+        x=Math.floor(Math.random()*this.width);
+        y=Math.floor(Math.random()*this.height);
+        united=true;
+        spinZero=this.getSpin(x,y,this.getRandomDirection());
+        for(qi=1;(qi<this.q) && (united);qi+=1) {
+            united=(spinZero==this.getSpin(x,y,this.getRandomDirection()));
+        }
+        this.globalSpin-=this.spinArray[y][x];
+        if(united) {
+            this.spinArray[y][x]=spinZero;
+        } else if(Math.random()<this.epsilon) {
+            this.spinArray[y][x]=-this.spinArray[y][x];
+        }
+        this.globalSpin+=this.spinArray[y][x];
+    }
+    getRandomDirection() {
+        let r=Math.floor(4*Math.random());
+        if(r==0) {
+            return [1,0];
+        }
+        if(r==1) {
+            return [-1,0];
+        }
+        if(r==2) {
+            return [0,-1];
+        }
+        if(r==3) {
+            return [0,1];
+        }
+        return [0,0];
+    }
+    getSpin(x,y,dir=[0,0]) {
+        let nx,ny;
+        nx=x+dir[0]+this.width;
+        ny=y+dir[1]+this.height;
+        return this.spinArray[(ny) % this.height][(nx) % this.width];
+    }
+}
---
+++ voter/qvoter-main.js
@@ -0,0 +1,83 @@
+function myParseFloat(val) {return parseFloat((""+val).replace(",","."));}
+
+let timeSeriesPlot=new plotlyPlot("timeSeries",["t","M(t)"]);
+
+let height=40;
+let width=100;
+let sqSize=5;
+let total=height*width;
+
+let model=null;
+let time=0;
+let timeSeries=null;
+let timeStep=100;
+let magSeries=null;
+
+let timeoutID=null;
+
+let g;
+
+function play() {
+    let i, mag;
+    for(i=0;i<64;i+=1) {
+        time+=timeStep;
+        mag=model.step(timeStep);
+
+        timeSeries.push(time);
+        magSeries.push(mag/total);
+    }
+    if(model.globalSpin==total || model.globalSpin==-total) {
+        $("#stop").click();
+    }
+}
+
+function plotFigures() {
+    timeSeriesPlot.update([timeSeries],[magSeries]);
+    plotField();
+}
+
+function plotField() {
+    let i,j;
+    for(i=0;i<model.height;i+=1) {
+        for(j=0;j<model.width;j+=1) {
+            if(model.spinArray[i][j]==1) g.fillStyle="rgb(255,0,0)";
+            else g.fillStyle="rgb(0,0,255)";
+            g.fillRect(j*sqSize,i*sqSize,sqSize,sqSize);
+        }
+    }
+}
+
+function seriesSetup() {
+    let i;
+    time=0;
+    timeSeries=new Array(1);
+    magSeries=new Array(1);
+    for(i=0;i<timeSeries.length;i+=1) {
+        timeSeries[i]=i-timeSeries.length;
+        magSeries[i]=model.globalSpin/total;
+    }
+}
+
+function setup() {
+    g=$("#plotDiv")[0].getContext("2d");
+    model=new QVoterModel(height,width,
+        myParseFloat($("#neighbors").val()),
+        myParseFloat($("#flip").val()),
+        myParseFloat($("#fillProb").val())
+    );
+    seriesSetup();
+}
+
+function frame() {
+    play();
+    plotFigures();
+}
+
+function stopGame() {
+    window.clearInterval(timeoutID);
+    timeoutID=null;
+}
+
+function resumeGame() {
+    timeoutID=window.setInterval("frame()",100.0);
+}
