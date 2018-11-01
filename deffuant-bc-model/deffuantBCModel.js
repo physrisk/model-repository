286d4e853b2f8b004f54684126320a6fa79314eb
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-11-01 14:50:07 +0200

    Deffuant variable tolerance model: initial commit

--- deffuant-bc-model/deffuantBCModel.js
+++ deffuant-bc-model/deffuantBCModel.js
@@ -1,10 +1,21 @@
 class DeffuantBCModel {
-    constructor(nAgents=100,mu=0.25,epsilon=0.25) {
+    constructor(nAgents=100,mu=0.25,epsilon=0.25,alpha=0,beta=0,epsilonMin=0.0) {
+        let norm;
         this.time=0;
         this.sense=0.003;
         this.nAgents=nAgents;
         this.mu=mu;
-        this.epsilon=epsilon;
+        if(alpha==0 && beta==0) {
+            this.epsilon=function (i) {
+                return epsilonMin+epsilon;
+            };
+        } else {
+            norm=epsilon*Math.pow(alpha,-alpha)*Math.pow(beta,-beta)*Math.pow(alpha+beta,alpha+beta);
+            this.epsilon=function (i) {
+                let x=this.opinions[i];
+                return epsilonMin+Math.pow(x,alpha)*Math.pow(1-x,beta)*norm;
+            };
+        }
         this.initializeOpinions();
     }
     step(tick=1) {
@@ -19,25 +30,25 @@ class DeffuantBCModel {
         return changes;
     }
     singleStep() {
-        let agent1Id, agent2Id, diff;
+        let agent1Id, agent2Id, diff, flipped;
         // pick random agents
         agent1Id=Math.floor(this.nAgents*Math.random());
         agent2Id=Math.floor(this.nAgents*Math.random());
         if(agent1Id==agent2Id) {
             return false;
         }
+        flipped=false;
         // compare the opinions
         diff=this.opinions[agent1Id]-this.opinions[agent2Id];
-        if(Math.abs(diff)<this.epsilon) {// influence
+        if(Math.abs(diff)<this.epsilon(agent1Id)) {// influence #1
             this.opinions[agent1Id]-=this.mu*diff;
-            this.opinions[agent2Id]+=this.mu*diff;
-            if(diff>this.sense) {
-                return true;
-            } else {
-                return false;
-            }
+            flipped=(diff>this.sense);
         }
-        return false;
+        if(Math.abs(diff)<this.epsilon(agent2Id)) {// influence #2
+            this.opinions[agent2Id]+=this.mu*diff;
+            flipped=(diff>this.sense);
+        }
+        return flipped;
     }
     initializeOpinions() {
         let i=0;
