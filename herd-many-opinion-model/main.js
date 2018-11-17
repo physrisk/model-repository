7a6619663611c7006b87cf666190c9de168341cc
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-11-17 14:36:33 +0200

    herd many state model: remove time series plot

--- herd-many-opinion-model/main.js
+++ herd-many-opinion-model/main.js
@@ -1,19 +1,15 @@
 function myParseFloat(val) {return parseFloat((""+val).replace(",","."));}
 function hexToDec(val) {return parseInt(val.toString(16),16);}
 
-let timeSeriesPlot=new plotlyPlot("timeSeries",["t","n(t)"]);
-let magPdfPlot=new plotlyPlot("magPdf",["n","p(n)"]);
+let magPdfPlot=new plotlyPlot("magPdf",["v","p(v)"]);
 
 let height=20;
-let width=50;
+let width=25;
 let total=height*width;
 let sqSize=10;
 let nStates=4;
 
 let model=null;
-let time=0;
-let timeSeries=null;
-let magSeries=null;
 
 let pdf=null;
 let pdfLen=0;
@@ -25,17 +21,11 @@ let colors=["#396ab1","#da7c30","#3e9651","#cc2529"];
 
 function play() {
     let i, j, mag;
-    timeSeries.splice(0,64);
-    magSeries.splice(0,64);
     for(i=0;i<64;i+=1) {
-        time+=1;
         mag=model.step().slice(0);
         for(j=0;j<mag.length;j+=1) {
             mag[j]/=total;
         }
-
-        timeSeries.push(time);
-        magSeries.push(mag.slice(0));
 
         for(j=0;j<nStates;j+=1) {
             pdf[Math.floor(mag[j]*pdf.length)][j]+=1;
@@ -48,19 +38,13 @@ function plotFigures() {
     let i,showPdf,showPdfX,showPdfY;
     showPdfX=new Array(nStates);
     showPdfY=new Array(nStates);
-    timeSeriesPlot.update([timeSeries,timeSeries,timeSeries,timeSeries],[
-        commonFunctions.toOneDimensionalArray(magSeries,0),
-        commonFunctions.toOneDimensionalArray(magSeries,1),
-        commonFunctions.toOneDimensionalArray(magSeries,2),
-        commonFunctions.toOneDimensionalArray(magSeries,3),
-    ],"lines",colors);
     if(pdfLen>128) {
         for(i=0;i<nStates;i+=1) {
             showPdf=commonFunctions.pdfModification(commonFunctions.toOneDimensionalArray(pdf,i),false,0,1,101,0,1/pdf.length,pdfLen);
             showPdfX[i]=commonFunctions.toOneDimensionalArray(showPdf,0);
             showPdfY[i]=commonFunctions.toOneDimensionalArray(showPdf,1).map(Math.log);
         }
-        magPdfPlot.update(showPdfX,showPdfY);
+        magPdfPlot.update(showPdfX,showPdfY,"lines",colors);
     } else {
         magPdfPlot.reset();
     }
@@ -103,21 +87,6 @@ function pdfSetup() {
     pdfLen=0;
 }
 
-function seriesSetup() {
-    let i,j,mag;
-    time=0;
-    timeSeries=new Array(4096);
-    magSeries=new Array(4096);
-    for(i=0;i<timeSeries.length;i+=1) {
-        timeSeries[i]=i-timeSeries.length;
-        mag=model.totals.slice(0);
-        for(j=0;j<mag.length;j+=1) {
-            mag[j]/=total;
-        }
-        magSeries[i]=mag.slice(0);
-    }
-}
-
 function setup() {
     let i,j,epsilonMatrix,tmp;
     epsilonMatrix=[];
@@ -129,9 +98,7 @@ function setup() {
         }
         epsilonMatrix.push(tmp);
     }
-    console.log(epsilonMatrix);
-    model=new HerdManyStateModel(total,nStates,epsilonMatrix,100/(total*total));
-    seriesSetup();
+    model=new HerdManyStateModel(total,nStates,epsilonMatrix,1/(total));
     pdfSetup();
 }
 
