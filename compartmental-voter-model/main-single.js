9c83b0588afeef27f856c86918540db73437e898
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2019-12-11 10:53:53 +0200

    single type compertmental voter model: add jStat and other improvements

--- compartmental-voter-model/main-single.js
+++ compartmental-voter-model/main-single.js
@@ -3,15 +3,16 @@ function myParseFloat(val) {return parseFloat((""+val).replace(",","."));}
 let rsPlot=new plotlyPlot("rsPlot",["r","N[r]"]);
 
 let model;
-let nAgents=[13,13];
+let nAgents=[20];
 let nComps=100;
-let capacity=30;
-let epsilon=[2,2];
+let capacity=2000;
+let epsilon=[2];
 
 let showX=[];
 let showY=[];
+let showTheory=[];
 
-let timeoutID=null;
+let runFlag=false;
 
 function play() {
     model.step(3e-6);
@@ -19,61 +20,110 @@ function play() {
 }
 
 function plotFigures() {
-    let sy;
+    let showRSD=showY.sort((a,b) => a<b);
+    rsPlot.setRanges([1,nComps],[0,showRSD[0]]);
+    rsPlot.update([showX,showX],[showRSD,showTheory],["markers","lines"],["#cc2525","#505050"]);
+}
 
-    sy=showY.sort((a,b) => a<b);
-
-    rsPlot.setRanges([1,nComps],[0,sy[0]]);
-    rsPlot.update([showX],[sy],"lines",["#396ab1"]);
+function getParams() {
+    nAgents=[parseInt($("#nAgents").val())];
+    epsilon=[myParseFloat($("#epsilon").val())];
+    nComps=parseInt($("#nComps").val());
+    capacity=parseInt($("#capacity").val());
 }
 
 function setup() {
     let i;
-    nAgents=[parseInt($("#nAgents").val())];
-    epsilon=[myParseFloat($("#epsilon").val())];
-    nComps=parseInt($("#nComps").val());
+    getParams();
     showX=new Array(nComps);
     for(i=0;i<nComps;i+=1) {
         showX[i]=i+1;
     }
-    capacity=parseInt($("#capacity").val());
-
+    if(capacity>=nAgents[0]*nComps) {
+        $("#alpha").val(epsilon[0]);
+        $("#beta").val((nComps-1)*epsilon[0]);
+    } 
+    onUpdateDistParams();
     model=new compVoterModel(nAgents,1,epsilon,nComps,capacity);
 }
 
 function frame() {
     play();
     plotFigures();
+    if(runFlag) {
+        setTimeout("frame()",30.0);
+    }
 }
 
 function stop() {
-    window.clearInterval(timeoutID);
-    timeoutID=null;
+    runFlag=false;
 }
 
 function resume() {
-    timeoutID=window.setInterval("frame()",100.0);
+    runFlag=true;
+    setTimeout("frame()",30.0);
+}
+
+function changeDistParamsInput() {
+    getParams();
+    if(capacity>=nAgents[0]*nComps) {
+        $("#alpha, #beta").attr("disabled","disabled");
+    } else if(!runFlag) {
+        $("#alpha, #beta").removeAttr("disabled");
+    }
+}
+
+function onUpdateEpsilon() {
+    getParams();
+    if(capacity>=nAgents[0]*nComps) {
+        $("#alpha").val(epsilon[0]);
+        $("#beta").val((nComps-1)*epsilon[0]);
+    }
+    onUpdateDistParams();
+}
+
+function onUpdateDistParams() {
+    getParams();
+    let a=myParseFloat($("#alpha").val());
+    let b=myParseFloat($("#beta").val());
+    showTheory=showX.map(v => capacity*jStat.ibetainv(v/nComps,a,b)).reverse();
+    plotFigures();
 }
 
 /* bind events and set initial GUI states */
 $("#stop").attr("disabled","disabled");
 $("#restart").click(function () {
-	setup();
-	$("#restart").attr("disabled","disabled");
-	$("#stop").removeAttr("disabled").click();
+    setup();
+    $("#restart, #alpha, #beta").attr("disabled","disabled");
+    $("#stop").removeAttr("disabled").click();
 });
 $("#stop").toggle(function() {
-	resume();
-	$("#stop").text("Stop");
-	$("#restart").attr("disabled","disabled");
+    resume();
+    $("#stop").text("Stop");
+    $("#restart, #alpha, #beta").attr("disabled","disabled");
 },function() {
-	stop();
-	$("#stop").text("Continue");
-	$("#restart").removeAttr("disabled");
+    stop();
+    $("#stop").text("Continue");
+    $("#restart").removeAttr("disabled");
+    changeDistParamsInput();
+});
+$("#capacity").change(function() {
+    changeDistParamsInput();
+});
+changeDistParamsInput();
+$("#epsilon").change(function() {
+    onUpdateEpsilon();
+});
+onUpdateEpsilon();
+$("#alpha, #beta").change(function() {
+    onUpdateDistParams();
+});
+$("#capacity").change(function() {
+    onUpdateDistParams();
 });
 
 /* onLoad */
 $(function () {
-	setup();
+    setup();
     plotFigures();
 });
