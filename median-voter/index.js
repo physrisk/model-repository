daae8db6b103c91c111bff06a5e24d90c84ac193
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2021-12-20 19:57:13 +0200

    strategic candidates in median voter model: fix errors

--- median-voter/index.js
+++ median-voter/index.js
@@ -7,61 +7,6 @@ let supporters_1 = Array(101).fill(null);
 let supporters_2 = Array(101).fill(null);
 
 let ai = { };
-function ai_reset() {
-    ai = {
-        previous: {
-            pos: null,
-            vote_diff: -999,
-        },
-        current: {
-            pos: null,
-            vote_diff: 0,
-        },
-        move_left: true,
-        move_right: true,
-        last_dir: 0,
-    };
-}
-ai_reset();
-
-function ai_move() {
-    if(ai.move_right || ai.move_left) {
-        if(ai.current.pos === null) {
-            ai.current.pos = my_parse_float(document.getElementById("position_p2").value);
-        }
-        let votes = calculate_votes();
-        ai.current.vote_diff = votes[1] - votes[0];
-        if(ai.current.vote_diff < ai.previous.vote_diff) {
-            ai.current.vote_diff = ai.previous.vote_diff;
-            ai.current.pos = ai.previous.pos;
-            if(ai.last_dir < 0) {
-                ai.move_left = false;
-            }
-            if(ai.last_dir > 0) {
-                ai.move_right = false;
-            }
-        } else {
-            ai.previous.vote_diff = ai.current.vote_diff;
-            ai.previous.pos = ai.current.pos;
-            if(Math.random() > 0.5) {
-                if(ai.move_right) {
-                    ai.current.pos = ai.current.pos + 0.01;
-                    ai.last_dir = 1;
-                }
-            } else {
-                if(ai.move_left) {
-                    ai.current.pos = ai.current.pos - 0.01;
-                    ai.last_dir = -1;
-                }
-            }
-            ai.current.pos = Math.min(1, Math.max(0, ai.current.pos));
-        }
-        document.getElementById("position_p2").value = ai.current.pos.toFixed(2);
-        update_support();
-        update_ui();
-    }
-    setTimeout(ai_move, 100);
-}
 
 function update_ui() {
     let position_p1 = my_parse_float(document.getElementById("position_p1").value);
@@ -77,7 +22,7 @@ function update_ui() {
         "lines",
         ["#222222", "#cc2222", "#cc2222", "#3366bb", "#3366bb"]);
     
-    votes = calculate_votes();
+    let votes = calculate_votes();
     document.getElementById("votes_p1").innerHTML = votes[0].toFixed(2);
     document.getElementById("votes_p2").innerHTML = votes[1].toFixed(2);
 }
@@ -120,6 +65,61 @@ function calculate_votes() {
     ];
 }
 
+function ai_reset() {
+    ai = {
+        previous: {
+            pos: null,
+            vote_diff: -999,
+        },
+        current: {
+            pos: null,
+            vote_diff: 0,
+        },
+        move_left: true,
+        move_right: true,
+        last_dir: 0,
+    };
+}
+
+function ai_move() {
+    if(ai.move_right || ai.move_left) {
+        if(ai.current.pos === null) {
+            ai.current.pos = my_parse_float(document.getElementById("position_p2").value);
+        }
+        let votes = calculate_votes();
+        ai.current.vote_diff = votes[1] - votes[0];
+        if(ai.current.vote_diff < ai.previous.vote_diff) {
+            ai.current.vote_diff = ai.previous.vote_diff;
+            ai.current.pos = ai.previous.pos;
+            if(ai.last_dir < 0) {
+                ai.move_left = false;
+            }
+            if(ai.last_dir > 0) {
+                ai.move_right = false;
+            }
+        } else {
+            ai.previous.vote_diff = ai.current.vote_diff;
+            ai.previous.pos = ai.current.pos;
+            if(Math.random() > 0.5) {
+                if(ai.move_right) {
+                    ai.current.pos = ai.current.pos + 0.01;
+                    ai.last_dir = 1;
+                }
+            } else {
+                if(ai.move_left) {
+                    ai.current.pos = ai.current.pos - 0.01;
+                    ai.last_dir = -1;
+                }
+            }
+            ai.current.pos = Math.min(1, Math.max(0, ai.current.pos));
+        }
+        document.getElementById("position_p2").value = ai.current.pos.toFixed(2);
+        update_support();
+        update_ui();
+    }
+    setTimeout(ai_move, 100);
+}
+
 document.getElementById("alpha").addEventListener("change", () => {
     generate_opinions();
     update_support();
@@ -158,5 +158,6 @@ document.getElementById("pragmatism").addEventListener("change", () => {
 generate_opinions();
 update_support();
 update_ui();
+ai_reset();
 
 setTimeout(ai_move, 100);
