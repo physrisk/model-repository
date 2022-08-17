44286ccb022833c75de7d4ff1ba2afdd3049c723
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2022-08-17 09:54:19 +0300

    parking: prettify

--- parking/index.js
+++ parking/index.js
@@ -1,12 +1,19 @@
-const my_parse_float = (val) => parseFloat((""+val).replace(",","."));
+const my_parse_float = (val) => parseFloat(("" + val).replace(",", "."));
 
-let ncars_plot = new plotlyPlot("ncarsPlot", ["events", "Total, Last"], [10,15,40,50]);
-let cost_plot = new plotlyPlot("costPlot", ["events", "norm. avg. Cost"], [10,15,40,50]);
+let ncars_plot = new plotlyPlot(
+    "ncarsPlot",
+    ["events", "Total, Last"],
+    [10, 15, 40, 50]
+);
+let cost_plot = new plotlyPlot(
+    "costPlot",
+    ["events", "norm. avg. Cost"],
+    [10, 15, 40, 50]
+);
 
 let canvas = document.getElementById("parking");
 let update_interval = 100;
 let continue_flag = false;
-let resume_flag = false;
 
 let stop_btn = document.getElementById("stop");
 let start_btn = document.getElementById("start");
@@ -16,29 +23,31 @@ let data = {
     n_cars: [],
     end_car: [],
     cost: [],
-}
+};
 
 let strategy = null;
 
 function plot_figures() {
     ncars_plot.update(
-        [data.time, data.time], [data.n_cars, data.end_car], "lines",
-        ["#2222cc", "#222222"],
+        [data.time, data.time],
+        [data.n_cars, data.end_car],
+        "lines",
+        ["#2222cc", "#222222"]
     );
-    cost_plot.update(
-        [data.time], [data.cost], "lines", ["#cc2222"],
-    );
+    cost_plot.update([data.time], [data.cost], "lines", ["#cc2222"]);
 }
 
 function draw_state() {
     let ctx = canvas.getContext("2d");
-    
+
     let size_sq = 10;
     let car_size = 10;
     let car_padding = 0;
 
-    let dims = [Math.floor(canvas.width/size_sq),
-                Math.floor(canvas.height/size_sq)];
+    let dims = [
+        Math.floor(canvas.width / size_sq),
+        Math.floor(canvas.height / size_sq),
+    ];
 
     // fill in with green background
     ctx.fillStyle = "#226622";
@@ -50,15 +59,15 @@ function draw_state() {
 
     // winding road
     ctx.fillStyle = "#cccccc";
-    for(let y=0; y<canvas.height-size_sq; y+=2*size_sq) {
-        if(y>0) {
+    for (let y = 0; y < canvas.height - size_sq; y += 2 * size_sq) {
+        if (y > 0) {
             ctx.fillRect(0, y, canvas.width, size_sq);
         } else {
-            ctx.fillRect(size_sq, y, canvas.width-size_sq, size_sq);
+            ctx.fillRect(size_sq, y, canvas.width - size_sq, size_sq);
         }
     }
-    for(let y=size_sq; y<canvas.height; y+=2*size_sq) {
-        if((y / size_sq) % 4 == 3) {
+    for (let y = size_sq; y < canvas.height; y += 2 * size_sq) {
+        if ((y / size_sq) % 4 == 3) {
             ctx.fillRect(0, y, size_sq, size_sq);
         } else {
             ctx.fillRect(canvas.width - size_sq, y, size_sq, size_sq);
@@ -67,26 +76,24 @@ function draw_state() {
 
     // parked cars
     model.states.forEach((v, i) => {
-        if(v>0) {
+        if (v > 0) {
             let pos = i + 1;
-            let h_pos = pos % (dims[0]+1);
-            let v_pos = 2*Math.floor(pos / (dims[0]+1));
-            let rotate = false;
-            let flip = ((v_pos / 2) % 2 == 1);
-            if(h_pos == dims[0]) {
+            let h_pos = pos % (dims[0] + 1);
+            let v_pos = 2 * Math.floor(pos / (dims[0] + 1));
+            let flip = (v_pos / 2) % 2 == 1;
+            if (h_pos == dims[0]) {
                 v_pos = v_pos + 1;
                 h_pos = h_pos - 1;
-                rotate = true;
             }
 
-            let x = h_pos*size_sq;
-            let y = v_pos*size_sq;
-            if(flip) {
+            let x = h_pos * size_sq;
+            let y = v_pos * size_sq;
+            if (flip) {
                 x = canvas.width - x - size_sq;
-                y = (v_pos+1)*size_sq - car_size - 2*car_padding;
+                y = (v_pos + 1) * size_sq - car_size - 2 * car_padding;
             }
-            ctx.fillStyle="#777777";
-            ctx.fillRect(x+car_padding, y+car_padding, car_size, car_size);
+            ctx.fillStyle = "#777777";
+            ctx.fillRect(x + car_padding, y + car_padding, car_size, car_size);
         }
     });
 }
@@ -94,10 +101,10 @@ function draw_state() {
 function append_data() {
     data.time.push(model.time);
     data.n_cars.push(model.n);
-    data.end_car.push(find_end(model.states)+1);
+    data.end_car.push(find_end(model.states) + 1);
     data.cost.push(model.get_mean_cost());
 
-    if(data.time.length > 1000) {
+    if (data.time.length > 1000) {
         data.time.shift(1);
         data.n_cars.shift(1);
         data.end_car.shift(1);
@@ -110,7 +117,7 @@ function run() {
     append_data();
     draw_state();
     plot_figures();
-    if(continue_flag) {
+    if (continue_flag) {
         setTimeout(run, update_interval);
     }
 }
@@ -118,39 +125,41 @@ function run() {
 start_btn.addEventListener("click", () => {
     start_btn.disabled = true;
     stop_btn.innerHTML = "Stop";
-    
-    let arrival_rate = my_parse_float(document.getElementById("arrival_rate").value);
+
+    let arrival_rate = my_parse_float(
+        document.getElementById("arrival_rate").value
+    );
     let walk_cost = my_parse_float(document.getElementById("walk_cost").value);
 
     model = new parkingModel(508, arrival_rate, 1, walk_cost, 1);
 
     let strategy_idx = parseInt(document.getElementById("strategy").value);
-    if(strategy_idx==0) {
+    if (strategy_idx == 0) {
         strategy = strategy_meek;
-    } else if(strategy_idx==1) {
+    } else if (strategy_idx == 1) {
         strategy = strategy_prudent;
-    } else if(strategy_idx==2) {
+    } else if (strategy_idx == 2) {
         strategy = strategy_optimist;
     } else {
         strategy = strategy_half;
     }
-    
+
     data = {
         time: [],
         n_cars: [],
         end_car: [],
         cost: [],
-    }
-    
+    };
+
     continue_flag = true;
     setTimeout(run, update_interval);
-    
+
     stop_btn.disabled = false;
 });
 stop_btn.addEventListener("click", () => {
     stop_btn.disabled = true;
     continue_flag = !continue_flag;
-    if(continue_flag) {
+    if (continue_flag) {
         setTimeout(run, update_interval);
         stop_btn.innerHTML = "Stop";
     } else {
